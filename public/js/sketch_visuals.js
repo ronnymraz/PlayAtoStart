c91f79e47a99c9e39fef0c19e86727fc2de2dbc2
pierredepaz <pierre.depaz@gmail.com>
2015-12-07 01:41:23 -0500

    main page basic css + about.html + howto.html + tutorial working + level 1

--- public/js/sketch_visuals.js
+++ public/js/sketch_visuals.js
@@ -4,10 +4,18 @@ var v = function(p){
 
 	var canDebug = false;
 
+	var handleNoteStartTime = p.millis();
+	var handleNoteTimer = 500; //wait half a second to listen for the second note
+	var canHandleNote = true;
+
 	var squaresNum = 10;
 	var squares = [];
 	var polygons = [];
 	var canChange = [];
+
+	var beat_stroke = 10;
+	var beat_alpha = 255;
+	var down_beat_alpha = 255;
 
 	var startTimeCorrectNote = p.millis();
 	//only draw a new note every given interval
@@ -81,12 +89,53 @@ var v = function(p){
 		}
 	}
 
-	correctNote = function(note){
-		console.log('correct note');
-		if(p.millis() - startTimeCorrectNote > timerCorrectNote){
-			var index = pickRandomIndex();
-			canChange[index] = true;
-			startTimeCorrectNote = p.millis();
+	setBeat = function(){
+		beat_stroke = 0;
+		down_beat_alpha = 155;
+	}
+
+	setDownBeat = function(){
+		beat_stroke = 0;
+		beat_alpha = 155;
+	}
+
+	drawBeat = function(){
+		p.strokeWeight(2);
+		p.stroke(150, beat_alpha);
+		p.noFill();
+		
+		for(var i = 0; i < 5; i++){
+			if(i % 2 == 0)
+				p.stroke(150, beat_alpha);
+			else
+				p.stroke(150, down_beat_alpha);
+
+			p.rect(0 + i*5, 0 + i*5, p.width-i*5*2, p.height-i*5*2);
+		}
+
+		p.strokeWeight(1);
+		p.line(0, p.height*0.5, p.width, p.height*0.5);
+
+		if(beat_alpha > 0){
+			beat_alpha -= 10;
+			beat_stroke += 3;
+		}
+
+		if(down_beat_alpha > 0)
+			down_beat_alpha -= 10;
+	}
+
+	handleCorrectNote = function(note){
+		if(canHandleNote){
+			console.log('handling correct note',note);
+			if(note == 'A')
+				canChange[pickRandomIndex()] = true;
+
+			if(note == 'G')
+				canChange[pickRandomIndex()] = true;
+
+			canHandleNote = false;
+			handleNoteStartTime = p.millis();
 		}
 	}
 
@@ -110,6 +159,7 @@ var v = function(p){
 		p.fill(255);
 		p.textAlign(p.RIGHT);
 		p.text ('reset - r', - 20 + p.windowWidth*0.5, 20 - p.windowHeight*0.5);
+		p.text ('mute - m '+mute, - 20 + p.windowWidth*0.5, 30 - p.windowHeight*0.5);
 		// p.text('current draw interval: ' + timerCorrectNote, 20 - p.windowWidth*0.5, 30 - p.windowHeight*0.5);
 		// p.text('current listen interval: ' + timerListenNote, 20 - p.windowWidth*0.5, 40 - p.windowHeight*0.5);
 	}
@@ -127,6 +177,9 @@ var v = function(p){
 
 	p.draw = function(){
 		p.background(50, 50, 55);
+
+		drawBeat();
+
 		p.fill(200);
 		p.translate(p.width*0.5, p.height*0.5);
 		for(var i = 0; i < squares.length; i++){
@@ -146,12 +199,20 @@ var v = function(p){
 			instructions();
 		}
 			
+		if(p.millis() - handleNoteStartTime > handleNoteTimer)
+			canHandleNote = true;
+
+		
 	};
 
 	p.keyPressed = function(){
 		if(p.key == ' '){
 			var i = Math.floor(Math.random()*canChange.length);
 			canChange[i] = true;
+		}
+
+		if(p.key == 'm' || p.key == 'M'){
+			mute = !mute;
 		}
 	}
 
